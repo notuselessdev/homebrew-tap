c1db7e110c96a0ed5dd6bddf846de76dc38c7879
Mayron Alves de Araujo <mayroonalves@gmail.com>
2026-03-16 19:02:49 -0600

    Update sha256 for rebuilt v0.1.0 (fixed binary name)

--- Formula/grove-street.rb
+++ Formula/grove-street.rb
@@ -7,20 +7,20 @@ class GroveStreet < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/notuselessdev/grove-street/releases/download/v#{version}/grove-street_darwin_arm64.tar.gz"
-      sha256 "7dc4eeae357069a21e5c08a23d8f91db901cf8d30d4727a9bc1b8cbeeee54463"
+      sha256 "101e13fe118b83bef8df240f7032fdfb6f7c9ded2ae7fb225e8d3a39e8632313"
     else
       url "https://github.com/notuselessdev/grove-street/releases/download/v#{version}/grove-street_darwin_amd64.tar.gz"
-      sha256 "a357ce9366b0b9c8c40d48165ef101fcd3f2a730a9516962b6dec33a4b79520f"
+      sha256 "6ea8c6ca37e71d2f69a4fa66d9080c124fe3c3bbf2e6a40de1bc863899d4db6f"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/notuselessdev/grove-street/releases/download/v#{version}/grove-street_linux_arm64.tar.gz"
-      sha256 "dd2148eaaad097d0f15d8bb61eb39dc878273524ffaa6f5110fca63580ce2417"
+      sha256 "3e3dcfe76fd6c1aca015b48fd24b8c5b247fd0c26702138d121464287cf825aa"
     else
       url "https://github.com/notuselessdev/grove-street/releases/download/v#{version}/grove-street_linux_amd64.tar.gz"
-      sha256 "15e5ed04c2c9e7fbefb8fe3591e3fede4e5a824c683567f89c154844036681a0"
+      sha256 "297f23d955f99cbf110a550c4484ec44405ab6c675e68e756cc51d72e99d0fbf"
     end
   end
 
