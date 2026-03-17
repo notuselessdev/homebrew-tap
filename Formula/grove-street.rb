4ce30e0124d96ec2e950271c0be8ee896b16204d
Mayron Alves de Araujo <mayroonalves@gmail.com>
2026-03-16 18:59:10 -0600

    Update sha256 checksums for v0.1.0

--- Formula/grove-street.rb
+++ Formula/grove-street.rb
@@ -7,20 +7,20 @@ class GroveStreet < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/notuselessdev/grove-street/releases/download/v#{version}/grove-street_darwin_arm64.tar.gz"
-      sha256 "PLACEHOLDER"
+      sha256 "7dc4eeae357069a21e5c08a23d8f91db901cf8d30d4727a9bc1b8cbeeee54463"
     else
       url "https://github.com/notuselessdev/grove-street/releases/download/v#{version}/grove-street_darwin_amd64.tar.gz"
-      sha256 "PLACEHOLDER"
+      sha256 "a357ce9366b0b9c8c40d48165ef101fcd3f2a730a9516962b6dec33a4b79520f"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/notuselessdev/grove-street/releases/download/v#{version}/grove-street_linux_arm64.tar.gz"
-      sha256 "PLACEHOLDER"
+      sha256 "dd2148eaaad097d0f15d8bb61eb39dc878273524ffaa6f5110fca63580ce2417"
     else
       url "https://github.com/notuselessdev/grove-street/releases/download/v#{version}/grove-street_linux_amd64.tar.gz"
-      sha256 "PLACEHOLDER"
+      sha256 "15e5ed04c2c9e7fbefb8fe3591e3fede4e5a824c683567f89c154844036681a0"
     end
   end
 
